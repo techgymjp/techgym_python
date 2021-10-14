af0c3677ae7ae315c1f620aaa08b13442c1f3e41
yokozawa0701 <d.yoko.0701@gmail.com>
2021-10-14 21:36:55 +0900

    自分の手がチョキ、相手の手がグーの場合 : 負け

--- bigginer.py
+++ bigginer.py
@@ -1,8 +1,8 @@
 print('じゃんけんスタート')
 print('自分の手を入力してください')
 
-my_hand = 0
-your_hand = 2
+my_hand = 1
+your_hand = 0
 
 if my_hand == 0:
     if your_hand == 0:
@@ -10,4 +10,7 @@ if my_hand == 0:
     elif your_hand == 1:
         print('勝ち')
     elif your_hand == 2:
+        print('負け')
+elif my_hand == 1:
+    if your_hand == 0:
         print('負け')
