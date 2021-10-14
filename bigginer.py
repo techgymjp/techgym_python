4741ec4553764becfd7653dd6cccc4a19658633d
yokozawa0701 <d.yoko.0701@gmail.com>
2021-10-14 21:40:05 +0900

    自分の手がパー、相手の手がグーの場合 : 勝ち

--- bigginer.py
+++ bigginer.py
@@ -1,8 +1,8 @@
 print('じゃんけんスタート')
 print('自分の手を入力してください')
 
-my_hand = 1
-your_hand = 2
+my_hand = 2
+your_hand = 0
 
 if my_hand == 0:
     if your_hand == 0:
@@ -17,4 +17,7 @@ elif my_hand == 1:
     elif your_hand == 1:
         print('あいこ')
     elif your_hand == 2:
+        print('勝ち')
+elif my_hand == 2:
+    if your_hand == 0:
         print('勝ち')
