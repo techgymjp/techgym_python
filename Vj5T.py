75067273a5acdce8aee6fb2eab238340f342fdad
techgym <tadashifujinaga@gmail.com>
2024-01-26 19:32:42 +0900

    Update Vj5T.py
    6章修正

--- Vj5T.py
+++ Vj5T.py
@@ -32,30 +32,6 @@ def play():
   load_image()
   show_test()
 
-def show_test():
-  plt.subplot(1,6,1)
-  plt.axis("off")
-  plt.imshow(card_images[20])
-  plt.subplot(1,6,2)
-  plt.axis("off")
-  plt.imshow(card_images[33])
-  plt.show()
-
-play()', allow_redirects=False)
-    with open(image_name, 'wb') as image:
-      image.write(response.content)
-   
-  img = cv.imread('./'+image_name)
-  img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
- 
-  h, w = img.shape[:2]
-  crop_img = img[:h // vsplit_number * vsplit_number, :w // hsplit_number * hsplit_number]
-  
-  card_images.clear()
-  for h_image in np.vsplit(crop_img, vsplit_number):
-    for v_image in np.hsplit(h_image, hsplit_number):
-      card_images.append(v_image)
-
 class Card:
   def __init__(self, mark, display_name, number, image):
     self.mark = mark
