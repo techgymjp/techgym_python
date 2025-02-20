ded0825d7e88a3bce6740a6b6a4712922b405c8f
techgym <tadashifujinaga@gmail.com>
2025-02-20 14:55:56 +0900

    Update Ck8N.py

--- Ck8N.py
+++ Ck8N.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
