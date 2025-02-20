0b7582ba819d32774f20443d2f764f8e1c354d2f
techgym <tadashifujinaga@gmail.com>
2025-02-20 14:49:50 +0900

    Update Gu4t.py

--- Gu4t.py
+++ Gu4t.py
@@ -1,6 +1,10 @@
 import requests
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
 url = 'https://techgym.jp/?cat=2'
 
-response = requests.get(url)
+response = requests.get(url,headers=headers)
 print(response.text)
