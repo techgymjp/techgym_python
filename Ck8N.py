df13e8776036b2e7c1c3be55f801783be10294d2
techgym <tadashifujinaga@gmail.com>
2025-02-20 14:51:59 +0900

    Update Ck8N.py

--- Ck8N.py
+++ Ck8N.py
@@ -1,9 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
 
 url = 'https://techgym.jp/?cat=2'
 
-response = requests.get(url)
+response = requests.get(url,headers=headers)
 soup = BeautifulSoup(response.text, 'lxml')
 
 articles = soup.find_all('div', class_='vk_post')
