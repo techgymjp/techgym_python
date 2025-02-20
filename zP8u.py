15b2fa8ba7f7abf8856cc3ba0bae68d3bb0fcfa3
techgym <tadashifujinaga@gmail.com>
2025-02-20 14:48:29 +0900

    Update zP8u.py

--- zP8u.py
+++ zP8u.py
@@ -1,3 +1,7 @@
 import requests
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+
 url = 'https://techgym.jp/?cat=2'
